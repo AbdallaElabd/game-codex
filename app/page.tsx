48aa3b906b2d9693a903bc5a4b3c590954aba412
Abdalla Elabd <abdalla.mohamed.elabd@gmail.com>
2023-05-31 22:36:24 +0200

    app router

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="p-16">
+      <h1 className="text-4xl font-thin text-gray-300 text-center">
+        Game RAWGic
+      </h1>
+    </div>
+  );
+}
